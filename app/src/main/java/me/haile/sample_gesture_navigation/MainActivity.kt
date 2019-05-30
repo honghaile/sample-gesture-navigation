57068fc8869ebd17b52530d542ba016124bd6743
Hai Le <honghaile@gmail.com>
2019-05-29 17:13:00 -0700

    initial commit

---
+++ app/src/main/java/me/haile/sample_gesture_navigation/MainActivity.kt
@@ -0,0 +1,23 @@
+package me.haile.sample_gesture_navigation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupUI()
+    }
+
+    private fun setupUI() {
+        root_view.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        root_view.setOnApplyWindowInsetsListener() { v, insets ->
+            insets.consumeSystemWindowInsets()
+        }
+    }
+}
