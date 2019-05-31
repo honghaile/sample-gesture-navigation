ad8eeaded1e174767d7563ed017b43f0ffa2b9f7
Hai Le <honghaile@gmail.com>
2019-05-31 09:26:49 -0700

    edge-to-edge immersive experience

--- app/src/main/java/me/haile/sample_gesture_navigation/MainActivity.kt
+++ app/src/main/java/me/haile/sample_gesture_navigation/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.haile.sample_gesture_navigation
 
 import android.os.Bundle
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,9 +16,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupUI() {
+        //root_view.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         root_view.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         root_view.setOnApplyWindowInsetsListener() { v, insets ->
             insets.consumeSystemWindowInsets()
         }
+
+        // override the back gesture, so the view continue to work
     }
 }
